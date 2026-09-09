09e4521670db6ab9e1775c1b82fdfff4d4db01cc
DevAbedHossain <devabedhossain@gmail.com>
2026-09-09 08:49:04 +0600

    Filter add country select

--- src/components/players/AvailablePlayers.tsx
+++ src/components/players/AvailablePlayers.tsx
@@ -10,15 +10,16 @@ export interface AvailablePlayersProps {
     setSelectedPlayers: Dispatch<SetStateAction<PlayerType[]>>
     searchPlayer: string
     setSearchPlayer: Dispatch<SetStateAction<string>>
+    countryPlayer: string
 }
 
-export default function AvailablePlayers({ players, coin, setCoin, selectedPlayers, setSelectedPlayers, searchPlayer, setSearchPlayer }: AvailablePlayersProps) {
+export default function AvailablePlayers({ players, coin, setCoin, selectedPlayers, setSelectedPlayers, searchPlayer, setSearchPlayer, countryPlayer }: AvailablePlayersProps) {
     
     return (
         <>
-            { searchPlayer.length > 0 ? <div className="grid grid-cols-3 gap-4">
+            { searchPlayer.length > 0 || countryPlayer.length > 0 ? <div className="grid grid-cols-3 gap-4">
             { 
-                players.filter(playerName => playerName.playerName.toLowerCase().includes(searchPlayer.toLowerCase())).map((player, index) => <PlayerCart key={ index} player={player} coin={coin} setCoin={setCoin} selectedPlayers={selectedPlayers} setSelectedPlayers={ setSelectedPlayers}></PlayerCart>)
+                players.filter(playerName => playerName.playerName.toLowerCase().includes(searchPlayer.toLowerCase()) || playerName.country === countryPlayer).map((player, index) => <PlayerCart key={ index} player={player} coin={coin} setCoin={setCoin} selectedPlayers={selectedPlayers} setSelectedPlayers={ setSelectedPlayers}></PlayerCart>)
             }
         </div> : <div className="grid grid-cols-3 gap-4">
             { 
--- src/components/players/players.tsx
+++ src/components/players/players.tsx
@@ -6,10 +6,10 @@ import SelectedPlayers from "./SelectedPlayers";
 export interface PlayersProps {
     playerDataPromise: Promise<PlayerType[]>
     coin: number
-    setCoin: Dispatch<SetStateAction<number >>
+    setCoin: Dispatch<SetStateAction<number>>
 }
 
-export default function Players({ playerDataPromise, coin, setCoin}: PlayersProps) {
+export default function Players({ playerDataPromise, coin, setCoin }: PlayersProps) {
 
     const players = use(playerDataPromise);
 
@@ -19,34 +19,48 @@ export default function Players({ playerDataPromise, coin, setCoin}: PlayersProp
 
     // Filter form logic
     const [searchPlayer, setSearchPlayer] = useState<string>("")
+    const [countryPlayer, setCountryPlayer] = useState<string>("")
 
-    const handleSearchPlayer = (e: ChangeEvent<HTMLInputElement>) => {
+    const handleSearchPlayer = (e) => {
         setSearchPlayer(e.target.value)
+
+        setCountryPlayer(e.target.value)
     }
 
-    const handelButtonType = (type: "available" | "selected") => { 
+    const handelButtonType = (type: "available" | "selected") => {
         setButtonType(type)
     }
-    
+
+    const countriesList = [...new Set(players.map(player => player.country))]
+
     return (
         <>
             <div className="container mx-auto space-y-10 my-10">
                 <div className="flex justify-between items-center">
                     <h2 className="text-2xl font-medium">{buttonType === "available" ? "Available Players" : "Selected Players"}</h2>
-                    <div>
-                        <input onChange={handleSearchPlayer} type="text" placeholder="Search Players..." className="input focus:border-none active:border-none w-2xs" />
+                    <div className="flex gap-2 items-center">
+                        <input onChange={handleSearchPlayer} type="text" placeholder="Search Players..." className="input focus:border-none active:border-none w-2xs outline-0" />
+                        <select onChange={handleSearchPlayer} name="country" id="country" className="select focus:border-none active:border-none w-2xs outline-0">
+                            <option value="">All Countries</option>
+                            {countriesList.map((country, index) => (
+                                <option key={index} value={country}>
+                                    {country}
+                                </option>
+                            ))}
+
+                        </select>
                     </div>
                     <div>
                         <button onClick={() => handelButtonType("available")} className={`btn rounded-r-none ${buttonType === "available" ? "btn-success" : ""}`}>Available</button>
-                        <button onClick={() => handelButtonType("selected")} className={`btn rounded-l-none ${buttonType === "selected" ? "btn-success" : ""}`}>Selected ({ mySelectedPlayers.length})</button>
+                        <button onClick={() => handelButtonType("selected")} className={`btn rounded-l-none ${buttonType === "selected" ? "btn-success" : ""}`}>Selected ({mySelectedPlayers.length})</button>
                     </div>
                 </div>
 
-                {buttonType === "available" ? <AvailablePlayers players={players} coin={coin} setCoin={setCoin} selectedPlayers={mySelectedPlayers} setSelectedPlayers={setMySelectedPlayers} searchPlayer={searchPlayer} setSearchPlayer={ setSearchPlayer}></AvailablePlayers> : <SelectedPlayers selectedPlayers={mySelectedPlayers} setSelectedPlayers={setMySelectedPlayers} coin={coin} setCoin={setCoin} searchPlayer={searchPlayer} setSearchPlayer={ setSearchPlayer}></SelectedPlayers>}
-                
-                
-        </div>
-            
+                {buttonType === "available" ? <AvailablePlayers players={players} coin={coin} setCoin={setCoin} selectedPlayers={mySelectedPlayers} setSelectedPlayers={setMySelectedPlayers} searchPlayer={searchPlayer} setSearchPlayer={setSearchPlayer} countryPlayer={ countryPlayer}></AvailablePlayers> : <SelectedPlayers selectedPlayers={mySelectedPlayers} setSelectedPlayers={setMySelectedPlayers} coin={coin} setCoin={setCoin} searchPlayer={searchPlayer} setSearchPlayer={setSearchPlayer}></SelectedPlayers>}
+
+
+            </div>
+
 
         </>
     )
